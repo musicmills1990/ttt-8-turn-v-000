dc849cc2b00c08f04713f294ea701e0b9f3f092d
Marc Mills <mdmills1990@gmail.com>
2018-08-01 21:04:24 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -26,6 +26,8 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+  user_input = gets.strip
+index = input_to_index(user_input)
   while valid_move?(board, index) == false
     turn(board)
     end
